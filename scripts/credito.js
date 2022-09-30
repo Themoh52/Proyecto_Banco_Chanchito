d62e9f937a59a7887967b5e93537045f407293d7
Themoh <them.olate@gmail.com>
2022-09-30 10:17:59 -0400

    Se introduce el valor mensual de las cuotas asociadas al crédito hipotecario

--- scripts/credito.js
+++ scripts/credito.js
@@ -15,19 +15,19 @@ do {
     if(pie>=(vivienda*0.2) && pie<=(vivienda*0.4)){
         let creditoa = vivienda*(1+tasaa);
         alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasaa*100)+"%,\n con un plazo de pago de 15 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoa+" Uf, equivalente a"+(uf*creditoa)+" pesos.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoa+" Uf, equivalente a"+(uf*creditoa)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditoa)/15)/12)+" pesos.");
     }else if(pie>=(vivienda*0.4) && pie<=(vivienda*0.6)){
         let creditob = vivienda*(1+tasab);
         alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasab*100)+"%,\n con un plazo de pago de 10 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditob+" Uf, equivalente a"+(uf*creditob)+" pesos.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditob+" Uf, equivalente a"+(uf*creditob)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditob))/10/12)+" pesos.");
     }else if(pie>=(vivienda*0.6) && pie<=(vivienda*0.8)){
         let creditoc = vivienda*(1+tasac);
         alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasac*100)+"%,\n con un plazo de pago de 8 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoc+" Uf, equivalente a"+(uf*creditoc)+" pesos."); 
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoc+" Uf, equivalente a"+(uf*creditoc)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditoc)/8)/12)+" pesos."); 
     }else if(pie>=(vivienda*0.8)){
         let creditod = vivienda*(1+tasad);
         alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasad*100)+"%,\n con un plazo de pago de 5 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditod+" Uf, equivalente a"+(uf*creditod)+" pesos."); 
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditod+" Uf, equivalente a"+(uf*creditod)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditod)/5)/12)+" pesos."); 
     }else{
         alert("Lo siento, pero si no tienes un pie mayor o igual al 20%, no es posible solicitar un crédito hipotecario con nosotros.");
     }
