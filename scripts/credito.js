9fdf2daf311da5b7531a8a4723b075c636e6c061
Themoh <them.olate@gmail.com>
2022-10-11 11:30:23 -0400

    Se introduce el uso de funciones de orden superior, y el uso de datos extraídos a partir  de arrays

--- scripts/credito.js
+++ scripts/credito.js
@@ -1,38 +1,53 @@
 //Simulador de crédito hipotecario//
-const tasaa = 0.05;
-const tasab = 0.0525;
-const tasac = 0.0555;
-const tasad = 0.0575;
+// Array con las tasas de interés, constante del valor de la UF, en CLP; y Array con los años de duración del crédito//
 const uf = 34245;
 const tasas =[0.05,0.0525,0.0555,0.0575];
+const anios = [5,8,10,15];
 
+//Función con las operaciones del Valor total del crédito, la Conversión de UF a peso, y el Valor de la Cuota Mensual//
+function operaciones(operacion){
+    if (operacion=="CreditoTotal"){
+        return (a,b)=> a*(1+b)
+    }else if (operacion=="Conversion"){
+        return (a)=> a*uf
+    }else if(operacion=="CuotaMensual"){
+        return (a,b) => (a/b)/12
+    }else if(operacion=="TasaInteres"){
+        return (a)=> a*100
+    }
+}    
+
+//Variables que contienen las funciones nacidas a partir de la función de orden superior//
+let ct = operaciones("CreditoTotal");
+let c = operaciones("Conversion");
+let cm = operaciones("CuotaMensual");
+let ti = operaciones("TasaInteres");
+
+//Saludo al usuario (Los modales hacen al programa)//
 let nombre = prompt("Hola, ingresa tu nombre");
 alert("Saludos "+nombre);
 
+//Función con el simulador del crédito, separado por rangos del valor del pie de la  vivienda//
 function credito(){
 do {
     vivienda = parseInt(prompt("Ingresa el valor de la vivienda en UF"));
     pie =parseInt(prompt("Ingresa el pie que tienes para la vivienda en UF. Recuerda que 1 Uf equivale a "+uf+" pesos"));
     if(pie>=(vivienda*0.2) && pie<=(vivienda*0.4)){
-        let creditoa = vivienda*(1+tasaa);
-        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasaa*100)+"%,\n con un plazo de pago de 15 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoa+" Uf, equivalente a"+(uf*creditoa)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditoa)/15)/12)+" pesos.");
+        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(ti(tasas[0]))+"%,\n con un plazo de pago de"+anios[3]+" años.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+ct(vivienda,tasas[0])+" Uf, equivalente a"+c(ct(vivienda,tasas[0]))+" pesos.\n El valor mensual de las cuotas a pagar son de "+cm(c(ct(vivienda,tasas[0])),anios[3])+" pesos.");
     }else if(pie>=(vivienda*0.4) && pie<=(vivienda*0.6)){
-        let creditob = vivienda*(1+tasab);
-        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasab*100)+"%,\n con un plazo de pago de 10 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditob+" Uf, equivalente a"+(uf*creditob)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditob))/10/12)+" pesos.");
+        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(ti(tasas[1]))+"%,\n con un plazo de pago de"+anios[2]+" años.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+ct(vivienda,tasas[1])+" Uf, equivalente a"+c(ct(vivienda,tasas[1]))+" pesos.\n El valor mensual de las cuotas a pagar son de "+cm(c(ct(vivienda,tasas[1])),anios[2])+" pesos.");
     }else if(pie>=(vivienda*0.6) && pie<=(vivienda*0.8)){
-        let creditoc = vivienda*(1+tasac);
-        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasac*100)+"%,\n con un plazo de pago de 8 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditoc+" Uf, equivalente a"+(uf*creditoc)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditoc)/8)/12)+" pesos."); 
+        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(ti(tasas[2]))+"%,\n con un plazo de pago de"+anios[1]+" años.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+ct(vivienda,tasas[2])+" Uf, equivalente a"+c(ct(vivienda,tasas[2]))+" pesos.\n El valor mensual de las cuotas a pagar son de "+cm(c(ct(vivienda,tasas[2])),anios[1])+" pesos."); 
     }else if(pie>=(vivienda*0.8)){
-        let creditod = vivienda*(1+tasad);
-        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(tasad*100)+"%,\n con un plazo de pago de 5 años.");
-        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+creditod+" Uf, equivalente a"+(uf*creditod)+" pesos.\n El valor mensual de las cuotas a pagar son de "+(((uf*creditod)/5)/12)+" pesos."); 
+        alert("Te ofrecemos un crédito hipotecario con una tasa de interés del "+(ti(tasas[3]))+"%,\n con un plazo de pago de"+anios[0]+" años.");
+        alert("Así, si solicitas un crédito con nosotros, este tendrá un valor total de "+ct(vivienda,tasas[3])+" Uf, equivalente a"+c(ct(vivienda,tasas[3]))+" pesos.\n El valor mensual de las cuotas a pagar son de "+cm(c(ct(vivienda,tasas[3])),anios[0])+" pesos."); 
     }else{
         alert("Lo siento, pero si no tienes un pie mayor o igual al 20%, no es posible solicitar un crédito hipotecario con nosotros.");
     }
-    respuesta = confirm("Y esa es la opción de crédito hipotecario que tenemos para tí. ¿Quieres realizar otra simulación?");
+    respuesta = confirm("Y esa es la opción de crédito hipotecario que tenemos para tí. ¿Quieres realizar otra simulación?,¿O desea solicitar la aprobación del crédito");
 } while (respuesta !=false)
 }
 credito();
--- scripts/cuenta_ahorro.js
+++ scripts/cuenta_ahorro.js
@@ -1,40 +1,43 @@
 //Simulador de cuenta de ahorro//
-const sueldoa = 450000;
-const sueldob = 650000;
-const sueldoc = 850000;
-const sueldod = 1250000;
+//Arrays con los rangos de sueldo, y las tasas de interés//
 const rangos = [450000,650000,850000,1250000];
+const tasas = [0.05,0.1,0.15,0.2];
 
+//Función de orden superior, que retorna las operaciones de incremento y tasa de interés// 
+function operaciones(operacion){
+    if (operacion=="Incremento"){
+        return (a,b)=> a*b
+    }else if (operacion=="TasaInteres"){
+        return (a)=> a*100
+    }    
+}
+//Variables que contienen a las funciones nacidas a partir de la función de orden superior//
+let i = operaciones("Incremento");
+let ti = operaciones("TasaInteres");
+
+//Saludo al usuario y solicitud del sueldo (los modales hacen al programa)//
 let nombre = prompt("Hola, ingresa tu nombre");
 alert("Saludos "+nombre);
 let sueldo = 0;
 function ahorro(){
 do {
     sueldo = parseInt(prompt("Ingresa tu sueldo"));
-    if(sueldo>=sueldoa && sueldo<=sueldob){
-        let tasaa = 0.2;
-        let incrementoa = sueldo*tasaa;
-        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+(tasaa*100)+"%");
-        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+incrementoa);
-    }else if(sueldo>=sueldob && sueldo<=sueldoc){
-        let tasab = 0.15;
-        let incrementob = sueldo*tasab;
-        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+(tasab*100)+"%");
-        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+incrementob);
-    }else if(sueldo>=sueldoc && sueldo<=sueldod){
-        let tasac = 0.1;
-        let incrementoc = sueldo*tasac;
-        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+(tasac*100)+"%");
-        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+incrementoc); 
-    }else if(sueldo>=sueldod){
-        let tasad = 0.05;
-        let incrementod = sueldo*tasad;
-        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+(tasad*100)+"%");
-        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+incrementod); 
+    if(sueldo>=rangos[0] && sueldo<=rangos[1]){
+        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+ti(tasas[3])+"%");
+        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+i(sueldo,tasas[3])+" pesos.");
+    }else if(sueldo>=rangos[1] && sueldo<=rangos[2]){
+        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+ti(tasas[2])+"%");
+        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+i(sueldo,tasas[2])+" pesos.");
+    }else if(sueldo>=rangos[2] && sueldo<=rangos[3]){
+        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+ti(tasas[1])+"%");
+        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+i(sueldo,tasas[1])+" pesos."); 
+    }else if(sueldo>=rangos[3]){
+        alert("Te ofrecemos una cuenta de ahorro con una tasa de interés del "+ti(tasas[0])+"%");
+        alert("Así, si depositas su sueldo con nosotros, mensualmente tus fondos tendrán un incremento equivalente a "+i(sueldo,tasas[0])+" pesos."); 
     }else{
         alert("Lo siento, pero tu sueldo no es suficiente para abrir una cuenta de ahorro con nosotros.");
     }
-    respuesta = confirm("Y esa es la opción de cuenta de ahorro que tenemos para tí. ¿Quieres realizar otra simulación? ");;
+    respuesta = confirm("Y esa es la opción de cuenta de ahorro que tenemos para tí. ¿Quieres realizar otra simulación?, ¿O deseas solicitar abrir una Cuenta de Ahorro con nosotros?");
 } while (respuesta !=false)
 }
 ahorro();
